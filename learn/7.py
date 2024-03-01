912e4d0495ab209842354ccd0de35ee1d1050138
BlazarWinGX <133990625+BlazarWinGX@users.noreply.github.com>
2024-03-01 22:49:06 +0800

    Update 7.py

--- learn/7.py
+++ learn/7.py
@@ -1,1 +1,13 @@
+# 第7章 用户输入和while循环
 
+# 7.1 input()函数的工作原理
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+#Tell me something, and I will repeat it back to you: hello
+#hello
+# 7.1.1 编写清晰的提示
+name = input("Please enter your name: ")
+print(f"\nHello,{name}!")
+#Please enter your name: eric
+
+#Hello,eric!
